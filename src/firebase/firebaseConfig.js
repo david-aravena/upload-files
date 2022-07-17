ef258791caf84e6955b81ef505be122db7624f63
davidoAraveno <david.aravenap@gmail.com>
2022-07-16 20:26:19 -0400

    firebase y firebase hosting instalados y configurados

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqyuckr7KOHBE1wxUbmznxgUB2r1nzavg",
+  authDomain: "ofis-cda85.firebaseapp.com",
+  projectId: "ofis-cda85",
+  storageBucket: "ofis-cda85.appspot.com",
+  messagingSenderId: "571743457392",
+  appId: "1:571743457392:web:1cc42a88daf052d0a264b5"
+};
+
+
+const app = initializeApp(firebaseConfig);
